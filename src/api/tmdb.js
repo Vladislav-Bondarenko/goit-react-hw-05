4fb01426499db2bb491a69ef6d980d95ff5f8c24
Vladislav Bondarenko <bondarenkovladislav2018@gmail.com>
2025-05-15 15:17:15 +0300

    Finish Code

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -8,3 +8,35 @@ const instance = axios.create({
     Authorization: `Bearer ${TOKEN}`,
   },
 });
+
+export async function getTrendingMovies() {
+  const response = await instance.get("/trending/movie/day");
+  return response.data.results;
+}
+
+export async function searchMovies(query) {
+  const response = await instance.get("/search/movie", {
+    params: {
+      query,
+      include_adult: false,
+      language: "en-US",
+      page: 1,
+    },
+  });
+  return response.data.results;
+}
+
+export async function getMovieDetails(id) {
+  const response = await instance.get(`/movie/${id}`);
+  return response.data;
+}
+
+export async function getMovieCast(id) {
+  const response = await instance.get(`/movie/${id}/credits`);
+  return response.data.cast;
+}
+
+export async function getMovieReviews(id) {
+  const response = await instance.get(`/movie/${id}/reviews`);
+  return response.data.results;
+}
